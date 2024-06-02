5428012373a74039f9fcad07d54a24728fbdc454
Hisham Titouh <hishamtitouh@proton.me>
2024-06-02 19:40:42 +0100

    Clean up

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,10 +11,10 @@ interface Props {
 
 function GameGrid({ gameQuery }: Props) {
   const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6];
-  if (error) return <Text>{error}</Text>;
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <>
+      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
